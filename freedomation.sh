8918018ffbaeaa47982b3f36f6f413552ee958a5
amiropale <amiropale@gmail.com>
2023-06-29 12:27:10 +0330

    Docker and dc-compose implemented.

--- freedomation.sh
+++ freedomation.sh
@@ -8,11 +8,32 @@ updating_comp() {
    sleep 4
 }
 
-installing_comp(){
+installing_comp() {
    echo "Gathering requirements to install..."
    sleep 1
    sudo apt-get install -y software-properties-common ufw wget curl git socat cron busybox bash-completion locales nano apt-utils
    echo "Installing components are finished."
+   sleep 4
+}
+
+installing_docker_dc_comp() {
+   read -rsn1 -p "Installing Docker and Docker-Compose for running services. Press any key to continue or Esc to abort procedure..." key # Prompt user to installing docker
+   if [[ $key == $'\x1b' ]]; then
+      echo -e "You chose to quitting this script and canceling procedure now.\nFor continuing to run x-ui on server we need permission to install Docker and its components to avoid interupting services.\nRe-run this bash script whenever you ready to install Docker on this machine!"
+      sleep 10
+      exit 0
+   else
+      sleep 5
+      sudo wget --quiet get.docker.com -O docker-setup.sh && sh docker-setup.sh
+      sleep 3
+      echo "Docker has been installed successfully on server. Now preparing to install Docker-Compose..."
+      sleep 3
+      LATEST_VERSION=$(curl --silent "https://api.github.com/repos/docker/compose/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+      sudo curl -L "https://github.com/docker/compose/releases/download/${LATEST_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+      sudo chmod +x /usr/local/bin/docker-compose
+      sleep 2
+      echo "Docker-Compose has been installed and configured successfully."
+   fi
 }
 
 MIN_PORT=49152
@@ -28,7 +49,7 @@ if grep -q "$OLD_PORT" /etc/ssh/sshd_config; then  # Check if sshd_config file h
       case "$response" in
          y|Y|Yes|YES|yes)
             read -p "Enter a port number for SSH (between $MIN_PORT and $MAX_PORT): " port 
-            if [[ $port =~ ^[0-9]+$ ]] && [[ $port -ge $MIN_PORT && $port -le $MAX_PORT ]]; then # Check if the input is a valid port number within the range
+            if [[ $port =~ ^[0-9]+$ ]] && ((port >= MIN_PORT))  &&  ((port <= MAX_PORT)); then # Check if the input is a valid port number within the range
                echo "Valid port number entered: $port. Preparing for change..."  
                sleep 2
                sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak # Create a backup
@@ -47,6 +68,7 @@ if grep -q "$OLD_PORT" /etc/ssh/sshd_config; then  # Check if sshd_config file h
                sleep 3
                updating_comp
                installing_comp
+               installing_docker_dc_comp
                break
             else 
                echo "Invalid port number. Please try again with a valid port within the range."
@@ -58,6 +80,7 @@ if grep -q "$OLD_PORT" /etc/ssh/sshd_config; then  # Check if sshd_config file h
             sleep 3
             updating_comp
             installing_comp
+            installing_docker_dc_comp
             break
             ;;
          *)
@@ -71,4 +94,5 @@ else
    sleep 3
    updating_comp
    installing_comp
+   installing_docker_dc_comp
 fi
