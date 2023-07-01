ed8ed6ad75a61d8310923f15a7a69bc6dc7ff5ae
amiropale <amiropale@gmail.com>
2023-07-01 12:56:39 +0330

    changing ssh port optimized and som changes.

--- freedomation.sh
+++ freedomation.sh
@@ -1,5 +1,76 @@
 #!/bin/bash
 
+sshd_change_check() {
+   MIN_PORT=49152
+   MAX_PORT=65535
+   current_port=$(grep -E "^Port" /etc/ssh/sshd_config | awk '{print $2}') # Get the current SSH port
+   default_port=22
+   echo -e "Checking SSH port has been changed recently or not...\n"
+   sleep 3
+   if [ "$current_port" -ne "$default_port" ]; then
+      echo -e "SSH port has been changed from the default port. Preparing for updating server components...\n"
+      sleep 3
+      updating_comp
+      installing_comp
+      installing_docker_dc_comp
+      acme_ssl
+   else
+      echo -e "SSH port is set to the default port.\n"
+      sleep 1
+      read -p "Do you want to change SSH port at first? (Recommanded) [Y/n] " response # Prompt the user for a port number
+      while true; do
+         case "$response" in
+            y|Y|Yes|YES|yes)
+               read -p "Enter a port number for SSH (between $MIN_PORT and $MAX_PORT): " port 
+               if [[ $port =~ ^[0-9]+$ ]] && ((port >= MIN_PORT))  &&  ((port <= MAX_PORT)); then # Check if the input is a valid port number within the range
+                  echo -e "Valid port number entered: $port. Preparing for change...\n"  
+                  sleep 2
+                  changing_port
+                  updating_comp
+                  installing_comp
+                  installing_docker_dc_comp
+                  acme_ssl
+                  break
+               else 
+                  echo -e "Invalid port number. Please try again with a valid port within the range.\n"
+                  sleep 2
+               fi   
+               ;;
+            n|N|No|NO|no)
+               echo -e "Strongly recommanded change your SSH port customizably later! Preparing for updating server components...\n"
+               sleep 3
+               updating_comp
+               installing_comp
+               installing_docker_dc_comp
+               acme_ssl
+               break
+               ;;
+            *)
+               echo -e "Invalid answer. Please input Y/y for changing port or N/n to avoid changing port now.\n"
+               sleep 2
+               ;;
+         esac
+      done
+   fi
+}
+
+changing_port() {
+   sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak # Create a backup
+   if [ $? -eq 0 ]; then # Check if the backup was successful
+      echo -e "Backup of SSHD Config created successfully.\n"
+   else
+      echo -e "Backup of SSHD Config creation failed.\n"
+   fi
+   sleep 2
+   sudo sed -i "s/$current_port/Port $port/g" /etc/ssh/sshd_config
+   echo -e "Changing SSH port done. Reloading service...\n"
+   sleep 2
+   sudo systemctl reload sshd
+   sleep 2
+   echo -e "Everything done. Preparing for updating server components...\n"
+   sleep 3
+}
+
 updating_comp() {
    echo "Press Enter whenever prompted to perform default actions."
    sleep 5
@@ -61,9 +132,6 @@ acme_ssl() {
    sudo shutdown -r now
 }
 
-MIN_PORT=49152
-MAX_PORT=65535
-OLD_PORT="#Port 22"
 
 #=====================================START-HINT=========================================
 echo "---------------------------------------------------------------------------------"
@@ -91,68 +159,10 @@ echo "--------------------------------------------------------------------------
 
 read -rsn1 -p "Press any key to continue or ESC to exit..." key
    if [[ $key == $'\x1b' ]]; then
-   echo -e "\nExiting..."
-   sleep 2
-   exit 0
-   fi
-
-sleep 1
-echo -e "\nChecking SSH port has been changed lately or not..."
-sleep 3
-if grep -q "$OLD_PORT" /etc/ssh/sshd_config; then  # Check if sshd_config file has been changed yet or not
-   while true; do
-      read -p "Do you want to change SSH port at first? (Recommanded) [y/n] " response # Prompt the user for a port number
+      echo -e "\nExiting..."
+      sleep 2
+      exit 0
+   else
       sleep 1
-      case "$response" in
-         y|Y|Yes|YES|yes)
-            read -p "Enter a port number for SSH (between $MIN_PORT and $MAX_PORT): " port 
-            if [[ $port =~ ^[0-9]+$ ]] && ((port >= MIN_PORT))  &&  ((port <= MAX_PORT)); then # Check if the input is a valid port number within the range
-               echo "Valid port number entered: $port. Preparing for change..."  
-               sleep 2
-               sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak # Create a backup
-                  if [ $? -eq 0 ]; then # Check if the backup was successful
-                     echo "Backup of SSHD Config created successfully."
-                  else
-                     echo "Backup of SSHD Config creation failed."
-                  fi
-               sleep 2
-               sudo sed -i "s/$OLD_PORT/Port $port/g" /etc/ssh/sshd_config
-               echo "Changing SSHD port done. Reloading service..."
-               sleep 2
-               sudo systemctl reload sshd
-               sleep 2
-               echo "Everything done. Preparing for updating server components..."
-               sleep 3
-               updating_comp
-               installing_comp
-               installing_docker_dc_comp
-               acme_ssl
-               break
-            else 
-               echo "Invalid port number. Please try again with a valid port within the range."
-               sleep 2
-               fi   
-            ;;
-         n|N|No|NO|no)
-            echo "Strongly recommanded change SSH port customizably later! Preparing for updating server components... "
-            sleep 3
-            updating_comp
-            installing_comp
-            installing_docker_dc_comp
-            acme_ssl
-            break
-            ;;
-         *)
-            echo "Invalid answer. Please input Y/y for changing port or N/n to avoid changing port now."
-            sleep 2
-            ;;
-      esac
-   done
-else
-   echo "You have been changed default SSH port on this server! Preparing for updating server components..."
-   sleep 3
-   updating_comp
-   installing_comp
-   installing_docker_dc_comp
-   acme_ssl
-fi
+      sshd_change_check
+   fi
